f837c20138f56442e45b9ee910c6360ea2e1d42a
Nikolaus.Stift <nstift.work@gmail.com>
2023-07-18 14:10:08 +0200

    Anpassung Hide Keyboard in detail view

--- app/src/main/java/com/example/workouttracker/DetailActivity.kt
+++ app/src/main/java/com/example/workouttracker/DetailActivity.kt
@@ -1,12 +1,12 @@
 package com.example.workouttracker
 
-import android.content.DialogInterface
+
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -18,19 +18,26 @@ import com.google.android.material.snackbar.Snackbar
 
 class DetailActivity : AppCompatActivity() {
 
-
+    private lateinit var notesEditText: EditText
+    private lateinit var repsEditText: EditText
+    private lateinit var weightEditText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         val exerciseName = findViewById<TextView>(R.id.exerciseName)
         exerciseName.text = intent.getStringExtra(Intent.EXTRA_TEXT)
         val addExercise = findViewById<FloatingActionButton>(R.id.add_exercise)
+
+        notesEditText = findViewById(R.id.notes)
+        repsEditText = findViewById(R.id.input_reps)
+        weightEditText = findViewById(R.id.input_weight)
+
         addExercise.setOnClickListener {
             showAlertDialog()
+            hideKeyboardForAllFields()
         }
 
-        val notesTextView = findViewById<EditText>(R.id.notes)
-        notesTextView.addTextChangedListener(object : TextWatcher {
+        notesEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
@@ -58,8 +65,17 @@ class DetailActivity : AppCompatActivity() {
     }
 
     fun showSnackbar(msg: String) {
-        var detailLayout = findViewById<ConstraintLayout>(R.id.detail_main)
+        val detailLayout = findViewById<ConstraintLayout>(R.id.detail_main)
         Snackbar.make(detailLayout, msg, Snackbar.LENGTH_SHORT).show()
+    }
 
+    private fun hideKeyboardForAllFields() {
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+
+        inputMethodManager.hideSoftInputFromWindow(notesEditText.windowToken, 0)
+
+        inputMethodManager.hideSoftInputFromWindow(repsEditText.windowToken, 0)
+
+        inputMethodManager.hideSoftInputFromWindow(weightEditText.windowToken, 0)
     }
 }
