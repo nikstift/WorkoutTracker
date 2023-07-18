06c132b47a5f30a7f9d1c660b1ffbdefb07ae34d
Nikolaus.Stift <nstift.work@gmail.com>
2023-07-18 16:38:53 +0200

    Gradle GSON hinzugefügt

--- app/src/main/java/com/example/workouttracker/DetailActivity.kt
+++ app/src/main/java/com/example/workouttracker/DetailActivity.kt
@@ -13,6 +13,12 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import java.text.SimpleDateFormat
 import java.util.Date
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 
 
 class DetailActivity : AppCompatActivity() {
@@ -21,6 +27,8 @@ class DetailActivity : AppCompatActivity() {
     private lateinit var repsEditText: EditText
     private lateinit var weightEditText: EditText
     private val sets = mutableListOf<ExerciseSet>()
+    private lateinit var sharedPref: SharedPreferences
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,9 +38,16 @@ class DetailActivity : AppCompatActivity() {
         exerciseName.text = intent.getStringExtra(Intent.EXTRA_TEXT)
         val addExercise = findViewById<FloatingActionButton>(R.id.add_exercise)
 
+        sharedPref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
         notesEditText = findViewById(R.id.notes)
         repsEditText = findViewById(R.id.input_reps)
         weightEditText = findViewById(R.id.input_weight)
+
+        loadSetsFromSharedPreferences()
+        for (set in sets) {
+            Log.d("DetailActivity", "Repetitions: ${set.repetitions}, Weight: ${set.weight}, Date: ${set.date}, Exercise: ${set.exercise}, Notes: ${set.notes}")
+        }
+
 
         addExercise.setOnClickListener {
             showAlertDialog()
@@ -48,7 +63,7 @@ class DetailActivity : AppCompatActivity() {
         val exerciseName = intent.getStringExtra(Intent.EXTRA_TEXT) ?: ""
         val notes = notesEditText.text.toString()
 
-        val exerciseSet = ExerciseSet(repetitions, weight, currentDate, exerciseName, notes)
+        val exerciseSet = ExerciseSet(repetitions,weight,currentDate,exerciseName,notes)
 
         val mdialog = MaterialAlertDialogBuilder(this)
             .setCancelable(true)
@@ -56,6 +71,7 @@ class DetailActivity : AppCompatActivity() {
             .setMessage("Do you want to add $repetitions reps of $weight KG of the exercise?")
             .setPositiveButton("Add set") { dialog, which ->
                 sets.add(exerciseSet)
+                saveSetsToSharedPreferences(sets)
                 showSnackbar("Awesome, well done!") }
             .setNegativeButton("Cancel") { dialog, which -> showSnackbar("Too bad, go harder!") }
         mdialog.show()
@@ -65,6 +81,26 @@ class DetailActivity : AppCompatActivity() {
         val detailLayout = findViewById<ConstraintLayout>(R.id.detail_main)
         Snackbar.make(detailLayout, msg, Snackbar.LENGTH_SHORT).show()
     }
+
+    private fun saveSetsToSharedPreferences(sets: List<ExerciseSet>) {
+        val gson = Gson()
+        val setsJson = gson.toJson(sets)
+
+        sharedPref.edit().putString("sets", setsJson).apply()
+    }
+
+    private fun loadSetsFromSharedPreferences() {
+        val setsJson = sharedPref.getString("sets", null)
+
+        if (setsJson != null) {
+            val gson = Gson()
+            val setType = object : TypeToken<List<ExerciseSet>>() {}.type
+            val loadedSets = gson.fromJson<List<ExerciseSet>>(setsJson, setType)
+            sets.addAll(loadedSets)
+        }
+    }
+
+
 
     private fun hideKeyboardForAllFields() {
         val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
